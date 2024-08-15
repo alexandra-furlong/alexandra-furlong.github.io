c21e0b1f187213dd7db36d196c039679d4a65d56
Alex Furlong <44102531+lizard-grandpa@users.noreply.github.com>
2024-08-14 17:02:49 -0700

    added nav bar, about me section and background for future icon

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./public/alexandra_furlong.html"],
+  theme: {
+    fontFamily:{
+      monstserrat: ['Montserrat', 'sans-serif'],
+      inter: ['Inter', 'sans-serif'],
+      primary: ['Open Sans', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        'primary': '#ff9fc4',
+        // 'primary': '#FFB6C1',
+        'background': "#141A1A",
+      },
+    },
+  },
+  plugins: [],
+}
+
